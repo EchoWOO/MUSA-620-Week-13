ea4b464ee577ef60afb54631511c878de2bcbbc7
Max Galka <galkamaxd@gmail.com>
2018-04-17 10:19:46 -0400

    Add files via upload

---
+++ app2b/server.R
@@ -0,0 +1,80 @@
+
+
+
+library(shiny)
+library(maps)
+library(dplyr)
+library(leaflet)
+library(DT)
+
+
+bigcities <- filter(world.cities, pop > 1000000)
+
+
+shinyServer(function(input, output, session) {
+  
+  
+  output$selectCountry <- renderUI({
+    countrylist <- arrange(bigcities, country.etc)
+    countrylist <- unique(countrylist$country.etc)
+    countrylist <- c("All",countrylist)
+    selectInput("country", "Select a country", as.list(countrylist))
+  })
+  
+  
+  output$selectCity <- renderUI({
+    
+    if(is.null(input$country) || input$country == "All") {
+      bigcitiesfiltered <- bigcities
+    } else {
+      bigcitiesfiltered <- filter(bigcities,country.etc == input$country)  
+    }
+  
+    selectInput("city", "Select a city", as.list(bigcitiesfiltered$name))
+    
+  })
+  
+  
+  
+  
+  output$table <- renderDataTable({
+    
+    if(is.null(input$country) || input$country == "All") {
+      bigcitiesfiltered <- bigcities
+    } else {
+      bigcitiesfiltered <- filter(bigcities,country.etc == input$country)  
+    }
+    
+    return(bigcitiesfiltered)
+    
+  })
+  
+  
+  
+  output$mymap <- renderLeaflet({
+    
+
+    if(is.null(input$country) || input$country == "All") {
+      bigcitiesfiltered <- bigcities
+    } else {
+      bigcitiesfiltered <- filter(bigcities,country.etc == input$country)  
+    }
+    
+    
+    leaflet() %>%
+      addProviderTiles(providers$CartoDB.PositronNoLabels) %>%
+      addCircleMarkers(
+        lng = ~long,
+        lat = ~lat,
+        data = bigcitiesfiltered,
+        radius = ~sqrt(pop)/250,
+        color = "blue",
+        stroke = FALSE,
+        fillOpacity = 0.3,
+        label = ~name
+      )
+  })
+  
+  
+})
+
