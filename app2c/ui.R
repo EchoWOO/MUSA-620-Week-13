4b637551531f41dc7a182c7f5bf2639edbe57552
Max Galka <galkamaxd@gmail.com>
2018-04-17 16:23:08 -0400

    Add files via upload

---
+++ app2c/ui.R
@@ -0,0 +1,30 @@
+
+
+
+
+library(shiny)
+library(DT)
+library(leaflet)
+
+
+
+fluidPage(
+  
+  titlePanel("Cities with population > 1 million"),
+  
+  leafletOutput("mymap"),
+  hr(),
+  fluidRow(
+    column(4,offset=2,
+           
+           uiOutput("selectCountry")
+    ),
+    column(4,
+           uiOutput("selectCity")
+    )
+  ),
+  hr(),
+  dataTableOutput('table')
+)
+
+
